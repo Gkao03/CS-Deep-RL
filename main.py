b3d3d8b381c8817942d5cacd653dc18394c7f74c
Gkao03 <gorek@andrew.cmu.edu>
2023-04-04 05:55:41 -0400

    testing

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
 import pfrl
+import torch
+from models import FCN
+
+
+if __name__ == '__main__':
+    x = torch.randn(1, 1, 64, 64)
+    model = FCN(action_size=6)
+    policy, value = model(x)
+
+    print(policy.shape)
+    print(value.shape)
