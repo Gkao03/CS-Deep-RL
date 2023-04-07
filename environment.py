8e12face81ada7e80e1be98d007d6ac15f1a61f2
Gkao03 <gorek@andrew.cmu.edu>
2023-04-07 15:51:05 -0400

    reset method

--- environment.py
+++ environment.py
@@ -4,4 +4,9 @@ import numpy as np
 # mimic a "gym" environment (this does not inherit from gym.Env)
 class MyEnvironment():
     def __init__(self, dataloader):
-        pass
+        self.dataloader = iter(dataloader)
+
+    def reset(self):
+        image_state, self.image_target = next(self.dataloader)
+        return image_state
+
