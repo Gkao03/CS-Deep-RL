b7a9474093b1f08227e583396d4bd135fbf5ad74
Gkao03 <gorek@andrew.cmu.edu>
2023-04-08 17:40:06 -0400

    config

---
+++ config.py
@@ -0,0 +1,8 @@
+class Args:
+    def __init__(self):
+        self.tmax = 5
+        self.Tmax = 150000
+        self.w_filter_size = 33
+        self.lr_init = 1e-3
+        self.image_size = 64
+        self.gamma = 0.9
