fb850319a89fe06a4d366db429b82b706c9d39e0
Gkao03 <gorek@andrew.cmu.edu>
2023-04-09 21:33:05 -0400

    some more configs

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 class Args:
     def __init__(self):
         self.seed = 2023
+        self.data_dir = "test_dir/"
         self.tmax = 5
         self.Tmax = 150000
         self.w_filter_size = 33
@@ -9,3 +10,4 @@ class Args:
         self.gamma = 0.9
         self.n = self.image_size ** 2
         self.m = int(0.2 * self.n)
+        self.batch_size = 1
