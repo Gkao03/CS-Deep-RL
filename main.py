0e7b135bdc4a4dc878149ff067ced86b1959008c
Gkao03 <gorek@andrew.cmu.edu>
2023-04-09 21:34:32 -0400

    setup data stuff and Qinit

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import numpy as np
 from pfrl.agents import A2C
 from models import FCN
 from config import Args
+from utils import get_device
 from data import *
 
 
@@ -11,6 +12,15 @@ if __name__ == '__main__':
     args = Args()
     torch.manual_seed(args.seed)
     np.seed(args.seed)
+
+    # data related stuff
+    A = generate_A(args.m, args.n)
+    transform = get_transform(args.image_size)
+    dataset = MyCSDataset(args.data_dir, A, transform=transform)
+    dataloader = DataLoader(dataset, batch_size=args.batch_size, shuffle=True, num_workers=2)
+
+    # calc Qinit
+    Q_init = calc_Qinit(dataloader, device=get_device())
 
     x = torch.randn(1, 1, 64, 64)
     model = FCN(action_size=6)
