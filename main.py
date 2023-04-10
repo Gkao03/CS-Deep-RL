18d724f54b93e5d673788497832a7bac0aa0e219
Gkao03 <gorek@andrew.cmu.edu>
2023-04-09 21:13:59 -0400

    imports and seeding

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 import pfrl
 import torch
+import numpy as np
 from pfrl.agents import A2C
 from models import FCN
 from config import Args
+from data import *
 
 
 if __name__ == '__main__':
+    args = Args()
+    torch.manual_seed(args.seed)
+    np.seed(args.seed)
+
     x = torch.randn(1, 1, 64, 64)
     model = FCN(action_size=6)
     policy, value = model(x)
