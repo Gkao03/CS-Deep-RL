ec69c9982644d0a5e92ce6740d79ec884c588ed2
Gkao03 <gorek@andrew.cmu.edu>
2023-04-10 19:02:49 -0400

    action space

--- config.py
+++ config.py
@@ -12,3 +12,15 @@ class Args:
         self.n = self.image_size ** 2
         self.m = int(0.2 * self.n)
         self.batch_size = 1
+
+
+class ActionSpace:
+    def __init__(self):
+        # idx to action (multiply)
+        self.action_space = {0: 0.5,
+                             1: 0.7,
+                             2: 0.9,
+                             3: 1.0,
+                             4: 1.1,
+                             5: 1.3,
+                             6: 1.5,}
