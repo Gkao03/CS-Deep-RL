743fbcf373767e0c0468551a9e99248d91a050d8
Gkao03 <gorek@andrew.cmu.edu>
2023-04-10 22:02:52 -0400

    setup and define actions and dataloader iterator

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import torch.optim as optim
 import numpy as np
 from pfrl.agents import A2C
 from models import FCN
-from config import Args
+from config import Args, ActionSpace
 from utils import get_device
 from data import *
 
@@ -20,6 +20,7 @@ if __name__ == '__main__':
     transform = get_transform(args.image_size)
     dataset = MyCSDataset(args.data_dir, A, transform=transform)
     dataloader = DataLoader(dataset, batch_size=args.batch_size, shuffle=True, num_workers=2)
+    data_iterator = iter(dataloader)  # use iterator to get data
 
     # calc Qinit
     print("calculating Qinit...")
@@ -27,7 +28,35 @@ if __name__ == '__main__':
     print(f"Qinit shape: {Q_init.shape}")
 
     # define model and other parameters
+    actions = ActionSpace()
     model = FCN(action_size=6).to(device)
     optimizer = optim.Adam(model.parameters(), lr=args.lr_init)
     lr_lambda = lambda episode: (1 - episode / args.max_episode) ** 0.9
     scheduler = optim.lr_scheduler.LambdaLR(optimizer, lr_lambda=lr_lambda, verbose=True)
+
+    # start
+    T = 0
+    t = 1
+
+    while T <= args.Tmax:
+        # initialization
+        optimizer.zero_grad()
+        loss_theta_p = 0
+        loss_theta_v = 0
+        loss_w = 0
+        t_start = t
+
+        # obtain some data
+        try:
+            terminal_state, _, state_y = next(data_iterator)
+        except StopIteration:
+            data_iterator = iter(dataloader)
+            terminal_state, _, state_y = next(data_iterator)
+
+        state_x = torch.matmul(Q_init, state_y).reshape(-1, 1, args.image_size, args.image_size).to(device)
+
+        while t - t_start < args.tmax:
+            policy, value = model(state_x)
+            action = policy.sample()
+            t += 1
+            T += 1
