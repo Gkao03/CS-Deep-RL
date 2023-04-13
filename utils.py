f69551ad1f91ad93eb5fc89ddeed3b12d039d1c2
Gkao03 <gorek@andrew.cmu.edu>
2023-04-13 18:03:48 -0400

    fix

--- utils.py
+++ utils.py
@@ -7,13 +7,15 @@ def get_device():
 
 def get_min_max_data(Qinit, dataloader):
     for _, _, flat_y in dataloader:
-        min_val = flat_y.min()
-        max_val = flat_y.max()
+        matmul = torch.matmul(Qinit, flat_y)
+        min_val = matmul.min()
+        max_val = matmul.max()
         break
 
     for _, _, flat_y in dataloader:
-        min_val = min(min_val, flat_y.min())
-        max_val = max(max_val, flat_y.max())
+        matmul = torch.matmul(Qinit, flat_y)
+        min_val = min(min_val, matmul.min())
+        max_val = max(max_val, matmul.max())
 
     return min_val, max_val
 
