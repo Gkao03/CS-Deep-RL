74faa77ed4264878aad571faeabc63c10a5b2863
Gkao03 <gorek@andrew.cmu.edu>
2023-04-12 20:56:52 -0400

    update next state

--- main.py
+++ main.py
@@ -88,9 +88,10 @@ if __name__ == '__main__':
             values.append(value)
             rewards.append(reward)
 
-            # update counters
+            # update counters and curr state
             t += 1
             T += 1
+            curr_state = next_state
 
         # get reward map
         R = value.detach()  # keep gradient?
