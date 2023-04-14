d176158dc9a51d1c04d8e3a93f8fb3a258b6e3da
Gkao03 <gorek@andrew.cmu.edu>
2023-04-14 06:03:38 -0400

    increase action space

--- config.py
+++ config.py
@@ -22,10 +22,13 @@ class Args:
 class ActionSpace:
     def __init__(self):
         # idx to action (multiply)
-        self.action_space = {0: 0.5,
-                             1: 0.7,
-                             2: 0.9,
-                             3: 1.0,
-                             4: 1.1,
-                             5: 1.3,
-                             6: 1.5,}
+        self.action_space = {0: 0.3,
+                             1: 0.5,
+                             2: 0.7,
+                             3: 0.9,
+                             4: 1.0,
+                             5: 1.1,
+                             6: 1.3,
+                             7: 1.5,
+                             8: 1.7,
+                             9: 2.0}
