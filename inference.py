305561654d5f603cf42a0ee8f9f33cb416e74077
Gkao03 <gorek@andrew.cmu.edu>
2023-04-14 21:40:31 -0400

    reconstruct

--- inference.py
+++ inference.py
@@ -11,6 +11,46 @@ from utils import get_device, get_min_max_data, rescale_tensor_01
 from data import *
 
 
+def reconstruct(model, reward_conv, Q_init, min_val, max_val, tmax, dataloader, actions, device, out_dir):
+    model = model.to(device)
+    reward_conv = reward_conv.to(device)
+    model.eval()
+    reward_conv.eval()
+
+    # just get 1 image from dataloader
+    for target_state, _, state_y in dataloader:
+        curr_state = torch.matmul(Q_init, state_y).reshape(-1, 1, args.image_size, args.image_size)
+        curr_state = rescale_tensor_01(curr_state, min_val, max_val)
+
+        for _ in range(tmax):
+            curr_state = curr_state.to(device)
+
+            # feed through network
+            policy, _ = model(curr_state)
+
+            # sample and get action
+            action_idx = policy.sample()
+            action = action_idx.clone().detach().cpu().float()
+            action.apply_(lambda x: actions[int(x)])
+            action = torch.unsqueeze(action, dim=1)
+
+            # get next_state
+            next_state = curr_state.detach().cpu() * action
+            curr_state = next_state
+
+        # save images
+        original = target_state.detach().cpu().numpy().squeeze()
+        reconstructed = curr_state.detach().cpu().numpy().squeeze()
+
+        print(f"original shape: {original.shape}")
+        print(f"reconstructed shape: {reconstructed.shape}")
+        print(f"original min: {original.min()}, max: {original.max()}")
+        print(f"reconstructed min: {reconstructed.min()}, max: {reconstructed.max()}")
+        break
+
+    print("DONE")
+
+
 if __name__ == "__main__":
     args = Args()
     torch.manual_seed(args.seed)
@@ -39,3 +79,9 @@ if __name__ == "__main__":
     # load state dicts from trained models
     model.load_state_dict(torch.load(os.path.join(args.out_dir, "model.pth")))
     reward_conv.load_state_dict(torch.load(os.path.join(args.out_dir, "reward_conv.pth")))
+
+    # reconstruct
+    dataloader = DataLoader(dataset, batch_size=1, shuffle=True, num_workers=2)
+
+    # call reconstruction
+    reconstruct(model, reward_conv, Q_init, min_val, max_val, args.tmax, dataloader, actions, device, args.out_dir)
