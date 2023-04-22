741f7c4401b85f43b9d1f10052919283cd0236fb
Gkao03 <gorek@andrew.cmu.edu>
2023-04-21 21:42:16 -0400

    fix reg

--- data.py
+++ data.py
@@ -96,7 +96,7 @@ def calc_Qinit(dataloader, device="cpu"):
     Y = torch.stack(ys, dim=1).to(device)
 
     YYT = torch.matmul(Y, Y.t())
-    YYT_reg = YYT + torch.full_like(YYT, 0.1)
+    YYT_reg = YYT + 0.1 * torch.eye(YYT.shape[0])
     YYT_inv = torch.inverse(YYT_reg)
     XYT = torch.matmul(X, Y.t())
     Qinit = torch.matmul(XYT, YYT_inv)
